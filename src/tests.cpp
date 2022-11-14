8545b91c7257e0b1589f4d9ce41d397179f7f8ee
Mike-devs <michael.elbaki@gmail.com>
2022-11-14 11:05:17 +0100

    First test with MongoDB retrieval, must be Mongo < 5.x!!!

--- src/tests.cpp
+++ src/tests.cpp
@@ -15,6 +15,9 @@
 #include "Poco/Exception.h"
 #include <iostream>
 
+#include "Poco/MongoDB/MongoDB.h"
+#include "Poco/MongoDB/Connection.h"
+#include "Poco/MongoDB/Cursor.h"
 
 using Poco::UUID;
 using Poco::UUIDGenerator;
@@ -38,12 +41,36 @@ int main(int argc, char** argv)
 		else
 			uuid = UUIDGenerator::defaultGenerator().createFromName(UUID::uri(), arg);
 
+		UUID::Version vers = uuid.version();
 		std::cout << uuid.toString() << std::endl;
 	}
 	catch (Exception& exc)
 	{
 		std::cerr << exc.displayText() << std::endl;
 		return 1;
+	}
+
+	Poco::MongoDB::Connection connection("localhost", 27017);
+	Poco::MongoDB::Cursor cursor("xtralife", "games");
+	// Selecting fields is done by adding them to the returnFieldSelector
+	// Use 1 as value of the element.
+	//cursor.query().returnFieldSelector().add("appid", 1);
+	Poco::MongoDB::ResponseMessage& response = cursor.next(connection);
+	for (;;)
+	{
+		for (Poco::MongoDB::Document::Vector::const_iterator it = response.documents().begin(); it != response.documents().end(); ++it)
+		{
+			std::cout << (*it)->get<std::string>("appid") << std::endl;
+		}
+
+		// When the cursorID is 0, there are no documents left, so break out ...
+		if (response.cursorID() == 0)
+		{
+			break;
+		}
+
+		// Get the next bunch of documents
+		response = cursor.next(connection);
 	}
 
 	return 0;
