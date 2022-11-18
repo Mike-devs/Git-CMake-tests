fb56358417e23afb4f7339b39d169a112eb3cecc
Mike-devs <michael.elbaki@gmail.com>
2022-11-18 15:51:52 +0000

    Add mongo-cxx-driver to linux and manage specific case where mongo-c-driver latest release does not compile

--- build.sh
+++ build.sh
@@ -5,22 +5,33 @@ else
 fi
 
 # Build and install all the dependencies
-for folder in poco mongo-c-driver
+for folder in poco mongo-c-driver mongo-cxx-driver
 do
+    if [ ! -d "$root_install_folder/ThirdParties/build/$folder" ]; then
+        mkdir -p "$root_install_folder/ThirdParties/build/$folder"
+    fi
+
+    if [[ $folder ==  "mongo-c-driver" ]] ; then
+        export SPECIFIC_PROJECT_FLAG="-DMONGOC_TEST_USE_CRYPT_SHARED=OFF"
+    else
+        export SPECIFIC_PROJECT_FLAG=
+    fi
+
+    pushd "$root_install_folder/ThirdParties/build/$folder"
+    cmake   "$root_install_folder/ThirdParties/src/$folder" \
+            -DCMAKE_CXX_STANDARD=17 \
+            -DCMAKE_INSTALL_PREFIX="$root_install_folder/ThirdParties/install/$folder" \
+            -DCMAKE_PREFIX_PATH="$cmake_prefix_path" \
+            $SPECIFIC_PROJECT_FLAG
+    cmake --build . --target install
+    popd
+
+    # Update the list of folders to retrieve CMake packages
     if [[ -v cmake_prefix_path ]] ; then
         export cmake_prefix_path="$cmake_prefix_path;$root_install_folder/ThirdParties/install/$folder"
     else
         export cmake_prefix_path="$root_install_folder/ThirdParties/install/$folder"
     fi
-
-    if [ ! -d "$root_install_folder/ThirdParties/build/$folder" ]; then
-        mkdir -p "$root_install_folder/ThirdParties/build/$folder"
-    fi
-
-    pushd "$root_install_folder/ThirdParties/build/$folder"
-    cmake "$root_install_folder/ThirdParties/src/$folder" -DCMAKE_INSTALL_PREFIX="$root_install_folder/ThirdParties/install/$folder"
-    cmake --build . --target install
-    popd
 done
 
 # Build our project
