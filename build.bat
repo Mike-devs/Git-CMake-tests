2f596c16001f4acc8d556aa2f268727486d1cfcd
Mike-devs <michael.elbaki@gmail.com>
2022-11-18 15:50:28 +0100

    Add mongo-cxx-driver dependency and checkout specific versions for each dependency

--- build.bat
+++ build.bat
@@ -7,16 +7,21 @@ if [%~1]==[] (
 )
 
 REM Build and install all the dependencies
-for %%a in (poco mongo-c-driver) do (
+for %%a in (poco mongo-c-driver mongo-cxx-driver) do (
     if not exist %root_install_folder%\ThirdParties\build\%%a mkdir %root_install_folder%\ThirdParties\build\%%a
     pushd %root_install_folder%\ThirdParties\build\%%a
+    "C:\Program Files\CMake\bin\cmake.exe"  %root_install_folder%\ThirdParties\src\%%a ^
+                                            -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /FS /EHsc" -DCMAKE_C_FLAGS="/FS" ^
+                                            -DCMAKE_INSTALL_PREFIX="%root_install_folder%\ThirdParties\install\%%a" ^
+                                            -DCMAKE_PREFIX_PATH="!cmake_prefix_path!"
+    "C:\Program Files\CMake\bin\cmake.exe" --build . --target install
+    popd
+
+    REM Update the list of folders to retrieve CMake packages
     if [!cmake_prefix_path!]==[] (
         set cmake_prefix_path=%root_install_folder%\ThirdParties\install\%%a) else (
         set cmake_prefix_path=!cmake_prefix_path!;%root_install_folder%\ThirdParties\install\%%a
     )
-    "C:\Program Files\CMake\bin\cmake.exe" %root_install_folder%\ThirdParties\src\%%a -DCMAKE_CXX_FLAGS="/FS /EHsc" -DCMAKE_C_FLAGS="/FS" -DCMAKE_INSTALL_PREFIX="%root_install_folder%\ThirdParties\install\%%a"
-    "C:\Program Files\CMake\bin\cmake.exe" --build . --target install
-    popd
 )
 
 REM Build our project
