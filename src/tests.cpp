a0e51b69ee34e5e5967679c9428772b723cbf61f
Mike-devs <michael.elbaki@gmail.com>
2022-11-23 18:17:47 +0100

    Add small server to return current time

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,10 +1,12 @@
 void GeneratePocoUuid(int argc, char** argv);
 void mongo_hello (int argc, char *argv[]);
+void LaunchTimeServer(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
 	GeneratePocoUuid(argc, argv);
 	mongo_hello(argc, argv);
+	LaunchTimeServer(argc, argv);
 
 	return 0;
 }
